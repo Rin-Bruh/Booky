fbf6e1f42ff13fa66cb4227d934201716484b653
Rin-Bruh <quytt.21it@vku.udn.vn>
2025-01-22 08:41:56 +0700

    ProductDTOAndProductDataStoreAndGetIndividualProduct

--- Booky_API/Controllers/ProductAPIController.cs
+++ Booky_API/Controllers/ProductAPIController.cs
@@ -1,20 +1,25 @@
-﻿using Booky_API.Models;
+﻿using Booky_API.Data;
+using Booky_API.Models;
+using Booky_API.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml;
 
 namespace Booky_API.Controllers
 {
+	//[Route("api/[controller]")]
 	[Route("api/ProductAPI")]
 	[ApiController]
 	public class ProductAPIController : ControllerBase
 	{
 		[HttpGet]
-		public IEnumerable<Products> GetProducts()
+		public IEnumerable<ProductDTO> GetProducts()
 		{
-			return new List<Products>
-			{
-				new Products { Id = 1, Title = "Cotton Candy"},
-				new Products { Id = 2, Title = "Rock in the Ocean" }
-			};
+			return ProductStore.productList;
+		}
+		[HttpGet("{id:int}")]
+		public ProductDTO GetProduct(int id)
+		{
+			return ProductStore.productList.FirstOrDefault(u=>u.Id==id);
 		}
 	}
 }
---
+++ Booky_API/Models/Dto/ProductDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Booky_API.Models.Dto
+{
+	public class ProductDTO
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+	}
+}
