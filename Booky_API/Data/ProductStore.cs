431816d23b27863742cfbefc6c710e431bff45c5
Rin-Bruh <quytt.21it@vku.udn.vn>
2025-01-23 14:33:28 +0700

    ModelStateValidationAndHttpDeletePutInAction

--- Booky_API/Data/ProductStore.cs
+++ Booky_API/Data/ProductStore.cs
@@ -6,8 +6,8 @@ namespace Booky_API.Data
 	{
 		public static List<ProductDTO> productList = new List<ProductDTO>
 			{
-				new ProductDTO { Id = 1, Title = "Cotton Candy"},
-				new ProductDTO { Id = 2, Title = "Rock in the Ocean" }
+				new ProductDTO { Id = 1, Title = "Cotton Candy", Description = "kkkk", ISBN = "WS3333333301", Author = "Abby Muscles"},
+				new ProductDTO { Id = 2, Title = "Rock in the Ocean", Description = "llll", ISBN = "SOTJ1111111101", Author = "Ron Parker"}
 			};
 	}
 }
--- Booky_API/Models/Dto/ProductDTO.cs
+++ Booky_API/Models/Dto/ProductDTO.cs
@@ -1,8 +1,17 @@
-﻿namespace Booky_API.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Booky_API.Models.Dto
 {
 	public class ProductDTO
 	{
 		public int Id { get; set; }
+		[Required]
+		[MaxLength(30)]
 		public string Title { get; set; }
+		public string Description { get; set; }
+		[Required]
+		public string ISBN { get; set; }
+		[Required]
+		public string Author { get; set; }
 	}
 }
