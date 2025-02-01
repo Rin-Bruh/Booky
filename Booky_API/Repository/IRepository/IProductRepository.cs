b870889e3a2fa876a46dcea96a559f73663fc5d3
Rin-Bruh <quytt.21it@vku.udn.vn>
2025-02-01 12:46:24 +0700

    AddProductRepositoryInterfaceAndProductRepositoryImplementation

---
+++ Booky_API/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Booky_API.Models;
+using System.Linq.Expressions;
+
+namespace Booky_API.Repository.IRepository
+{
+	public interface IProductRepository
+	{
+		Task<List<Product>> GetAll(Expression<Func<Product, bool>> filter = null);
+		Task<Product> Get(Expression<Func<Product, bool>> filter = null, bool tracked = true);
+		Task Create(Product entity);
+		Task Remove(Product entity);
+		Task Save();
+	}
+}
