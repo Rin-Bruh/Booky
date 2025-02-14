950d1eafc6011ddb25bf1ead4bbf7f913859f558
Rin-Bruh <quytt.21it@vku.udn.vn>
2025-02-14 10:55:37 +0700

    AddDTOsForLoginAndRegister-AddAuthService-AuthControllerActionMethods103

--- Booky_Web/Program.cs
+++ Booky_Web/Program.cs
@@ -14,6 +14,9 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddHttpClient<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 
+builder.Services.AddHttpClient<IAuthService, AuthService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Booky_Web/Services/AuthService.cs
@@ -0,0 +1,42 @@
+﻿using Booky_Utility;
+using Booky_Web.Models;
+using Booky_Web.Models.Dto;
+using Booky_Web.Services.IServices;
+
+namespace Booky_Web.Services
+{
+	public class AuthService : BaseService, IAuthService
+	{
+		private readonly IHttpClientFactory _clientFactory;
+		private string authUrl;
+
+		public AuthService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+		{
+			_clientFactory = clientFactory;
+			authUrl = configuration.GetValue<string>("ServiceUrls:BookyAPI");
+
+		}
+
+		public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+		{
+			return SendAsync<T>(new APIRequest()
+			{
+				ApiType = SD.ApiType.POST,
+				Data = obj,
+				Url = authUrl + "/api/UsersAuth/login"
+				//Token = token
+			});
+		}
+
+		public Task<T> RegisterAsync<T>(RegisterationRequestDTO obj)
+		{
+			return SendAsync<T>(new APIRequest()
+			{
+				ApiType = SD.ApiType.POST,
+				Data = obj,
+				Url = authUrl + "/api/UsersAuth/register"
+				//Token = token
+			});
+		}
+	}
+}
